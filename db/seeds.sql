882398031dfa0179bba8bee974fac660282f69f0
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-06-16 14:16:52 -0400

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,24 @@
-INSERT INTO department
-    (department)
-VALUES 
-    (),
-    (),
-    (),
-    ();
-
-INSERT INTO job
-    (title, salary, departmentID)
-VALUES 
-    (),
-    (),
-    (),
-    ();
-
-INSERT INTO employee
-    (firstN, lastN, jobID, managerID)
-VALUES 
-    (),
-    (),
-    (),
-    ();
+INSERT INTO department (department_name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Sales Lead', '100000', 1),
+    ('Salesperson', '80000', 1),
+    ('Lead Engineer', '150000', 2),
+    ('Software engineer', '120000', 2),
+    ('Accountant', '125000', 3),
+    ('Legal Team Lead', '250000', 4),
+    ('Lawyer', '190000', 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Ben', 'Askren', 1, 3),
+    ('Timmy', 'Benjamin', 2, 1),
+    ('Kevin', 'Spacey', 3, NULL),
+    ('Benjamin', 'Hoover', 4, 3),
+    ('Post', 'Malone', 5, NULL),
+    ('Kevin', 'Hart', 6, NULL),
+    ('Trisha', 'Doe', 7, 6);   
