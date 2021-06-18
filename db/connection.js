fba45ef247cc6d28cf8c83fef9342f0a118812fc
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-06-18 07:48:21 -0400

    updated root

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "Surfer10",
+        password: "",
         database: "employee_tracker",
     },
 );
